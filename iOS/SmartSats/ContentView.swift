78d92cf02ace6b9483920baa9cb16cf9b4e4907b
Jason <jayvdb1@gmail.com>
2023-07-06 20:58:36 +0200

    chore: ios project setup
    fix: gitignore

---
+++ iOS/SmartSats/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SmartSats
+//
+//  Created by Jason van den Berg on 2023/07/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOS/SmartSats/SmartSatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartSatsApp.swift
+//  SmartSats
+//
+//  Created by Jason van den Berg on 2023/07/06.
+//
+
+import SwiftUI
+
+@main
+struct SmartSatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
